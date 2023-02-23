708db30c443e541b787a359358f7b2200bd7d992
Gulam-Rabbi <124187060+Gulam-Rabbi@users.noreply.github.com>
2023-02-23 15:57:14 +0600

    code added

--- hw-oop-lab-main/Information.java
+++ hw-oop-lab-main/Information.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class Information {
     public static void main(String[] args) {
-        System.out.println("Name:"+ " Md.Tanvir Hasan Tonmoy");
-        System.out.println("ID:"+ " 221-15-5200");
-        System.out.println("Address:"+ " Mirpur,Dhaka,Bangladesh");
+        System.out.println("Name:"+ "Gulam Rabbi");
+        System.out.println("ID:"+ " 221-15-5039");
+        System.out.println("Address:"+ " Jamalpur,Mymensingh,Bangladesh");
         System.out.println("Department:"+ " CSE");
         System.out.println("Favorite Game:"+ " Velorant");
     }
